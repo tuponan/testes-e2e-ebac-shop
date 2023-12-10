978b897b77b62f23bbd8202a6cb679e64930098f
Tuponan mello <grandetupa@gmail.com>
2023-12-10 18:38:51 -0300

    exercicio cypress

--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -1,20 +1,30 @@
 /// <reference types="cypress" />
+import { fakerDE as faker } from '@faker-js/faker';
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-    /*  Como cliente 
-        Quero acessar a Loja EBAC 
-        Para fazer um pedido de 4 produtos 
-        Fazendo a escolha dos produtos
-        Adicionando ao carrinho
-        Preenchendo todas opções no checkout
-        E validando minha compra ao final */
+
 
     beforeEach(() => {
-        cy.visit('/')
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        //TODO 
+        let emailFaker = faker.internet.email()
+        var quantidade = 4
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type('teste@teste.com')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.get('[class="product-block grid"]')
+            .contains('Bruno Compete Hoodie').click()
+        cy.get('.button-variable-item-L').click()
+        cy.get('.button-variable-item-Black').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+
+        cy.get('.woocommerce-message').should('contains', 'adicionados no seu carrinho.')
     });
 
 
