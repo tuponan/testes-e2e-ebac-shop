2ba82e2d38353cb0c9b7bd7dd7d78debd484813c
Tuponan mello <grandetupa@gmail.com>
2023-12-20 13:42:06 -0300

    alteracoes

---
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress" />
+import { fakerDE as faker } from '@faker-js/faker';
+
+context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
+
+    beforeEach(() => {
+        cy.visit('minha-conta')
+    });
+
+    it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+        let emailFaker = faker.internet.email()
+
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type('teste@teste.com')
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addP('Apollo Running Short', '36', 'Black', 4)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addP('Aero Daily Fitness Tee', 'XL', 'Brown', 2)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addP('Aether Gym Pant', '34', 'Green', 3)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.addP('Ajax Full-Zip Sweatshirt', 'XS', 'Red', 1)
+        cy.get('#primary-menu > .menu-item-629 > a').click()
+
+        cy.get('.dropdown-toggle > .mini-cart-items').should
+      
+    });
+
+
+})
--- cypress/integration/exercicio-e2e.spec.js
+++ cypress/integration/exercicio-e2e.spec.js
@@ -5,27 +5,11 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
 
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('http://lojaebac.ebaconline.art.br')
     });
 
     it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-        let emailFaker = faker.internet.email()
-        var quantidade = 4
 
-        cy.get('#reg_email').type(emailFaker)
-        cy.get('#reg_password').type('teste@teste.com')
-        cy.get(':nth-child(4) > .button').click()
-        cy.get('#primary-menu > .menu-item-629 > a').click()
-
-        cy.get('[class="product-block grid"]')
-            .contains('Bruno Compete Hoodie').click()
-        cy.get('.button-variable-item-L').click()
-        cy.get('.button-variable-item-Black').click()
-        cy.get('.input-text').clear().type(quantidade)
-        cy.get('.single_add_to_cart_button').click()
-
-        cy.get('.woocommerce-message').should('contains', 'adicionados no seu carrinho.')
     });
-
 
 })
